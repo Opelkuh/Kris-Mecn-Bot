66c2f0636e897d082ffbd575bbcf6e9a8fe9e522
JVancata <vancata.kuba@seznam.cz>
2018-08-27 03:44:43 +0200

    👅 face choices!

--- chatCommands.js
+++ chatCommands.js
@@ -2,7 +2,18 @@ const dh = require("./discordHelper.js");
 
 function init(client) {
 	this.flex = new dh.Command("flex", (msg) => {
+		getRandomChoice = (array) => {
+			return array[Math.floor(Math.random() * array.length)];
+		}
+
 		let color = msg.splitContent[0];
+		let face = msg.splitContent[1];
+		
+		const colorChoices = ["", "pale", "cream", "moderate", "dark", "black"];
+		const faceChoices = ["tongue", "lips", "kiss"];
+		if (!color) color = getRandomChoice(colorChoices);
+		if (!face) face = getRandomChoice(faceChoices);
+
 		switch (color) {
 			case "pale":
 				color = "🏻";
@@ -19,12 +30,29 @@ function init(client) {
 			case "black":
 				color = "🏿";
 				break;
-			default: 
+			default:
 				color = "";
 				break;
 		}
-		msg.channel.send(`💪${color}👁️👅👁️💪${color}`);
-	}, "chat", "Flex on 'em! Usage: !flex <none;pale;cream;moderate;dark;black>");
+		switch (face) {
+			case "tongue":
+				face = "👅";
+				break;
+			case "lips":
+				face = "👄";
+				break;
+			case "kiss":
+				face = "💋";
+				break;
+			case "pig":
+				face = "🐽";
+				break;
+			default:
+				face = "👅";
+				break;
+		}
+		msg.channel.send(`💪${color}👁️${face}👁️💪${color}`);
+	}, "chat", "Flex on 'em! Usage: !flex <none;pale;cream;moderate;dark;black> <none;tongue;lips;kiss;pig>");
 	//Return
 	return this;
 }
