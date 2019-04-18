2022a596af8ad6c9d03bc2d06988263189b90600
mike89745 <mikes89745@gmail.com>
2019-04-18 17:09:18 +0200

    roll

--- chatCommands.js
+++ chatCommands.js
@@ -91,6 +91,17 @@ function init(client) {
 		problem.replace(":", "/");
 		msg.channel.send(`\`${problem}\`\n**Result:** \`${eval(problem)}\``);
 	}, "calc", "Easy calculator! Usage: !calc <problem>");
+
+	this.roll = new dh.Command("roll", (msg) => {
+		let rollRangeMin = 0;
+		let rollRangeMax = 100;
+		if(msg.splitContent.length > 3){
+			rollRangeMin = parseInt(msg.splitContent.split("-")[0]) ? parseInt(msg.splitContent.split("-")[0]) : 0;
+			rollRangeMax = parseInt(msg.splitContent.split("-")[1]) ? parseInt(msg.splitContent.split("-")[1]) : 100;
+		}
+		
+		msg.channel.send(`${msg.author.username} rolled ${Math.floor(Math.random() * rollRangeMax) + rollRangeMin  }`)
+	})
 	//Return
 	return this;
 }
